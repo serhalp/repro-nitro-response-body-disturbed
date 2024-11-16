122e2a9533d580ca7ff499704f65888321143d48
serhalp <philippe.serhal@gmail.com>
2024-11-16 16:44:29 -0500

    fix: stream response body too

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,7 +1,17 @@
 import { eventHandler, readBody } from 'h3';
 
 export default eventHandler(async (event) => {
-  const body = await readBody(event);
-  console.log('received request body:', body);
-  return 'success';
+  const requestBody = await readBody(event);
+  console.log('received request body:', requestBody);
+
+  const encoder = new TextEncoder();
+  const stream = new ReadableStream({
+    start(controller) {
+      controller.enqueue(encoder.encode("this is a "));
+      controller.enqueue(encoder.encode("streamed response"));
+      controller.close();
+    },
+  });
+
+  return new Response(stream);
 });
